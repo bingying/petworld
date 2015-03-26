886434bc62e2ea991e9b245d2f4a50f23e8e4395
liubing01 <liubing01@youku.com>
2015-03-26 22:52:35 +0800

    update codes

--- petgang-web/petgang-web/src/main/java/com/petgang/web/controller/IndexController.java
+++ petgang-web/petgang-web/src/main/java/com/petgang/web/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.petgang.web.controller;
 
+import java.util.Properties;
+
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +17,13 @@ public class IndexController {
     @Resource(name = "jsonView")
     private View jsonView;
 
+    @Autowired
+    private Properties configProperties;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index() {
         ModelAndView mav = new ModelAndView(jsonView);
-        mav.addObject("asdfasdf", "hahahha");
+        mav.addObject("asdfasdf", configProperties.getProperty("mysql.biz.master"));
         return mav;
     }
 }
