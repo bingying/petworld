6db2a9d44cb44a353a83fc79c5dd999faec85cc6
liubing01 <liubing01@youku.com>
2015-04-18 00:08:48 +0800

    update codes

--- petgang-commons/petgang-commons/src/main/java/com/petgang/commons/http/HttpUnit.java
+++ petgang-commons/petgang-commons/src/main/java/com/petgang/commons/http/HttpUnit.java
@@ -1,5 +1,6 @@
 package com.petgang.commons.http;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -9,11 +10,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.codec.Charsets;
+import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -23,28 +25,28 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 
 public class HttpUnit {
 
-    private static CloseableHttpClient client = null;
-    static {
-        BasicHttpClientConnectionManager manager = new BasicHttpClientConnectionManager();
-        manager.closeIdleConnections(5, TimeUnit.SECONDS);
-        client = HttpClients.createMinimal(manager);
-    }
+    private static CloseableHttpClient client = HttpClients.createDefault();
 
-    public Result get(String uri) throws ClientProtocolException, IOException {
+    private static RequestConfig REQUEST_CONFIG = RequestConfig.custom().setSocketTimeout(10000)
+            .setConnectTimeout(2000).setRedirectsEnabled(true).setConnectionRequestTimeout(10000)
+            .build();
+
+    public static Result get(String uri) throws ClientProtocolException, IOException {
         HttpGet get = new HttpGet(uri);
+        get.setConfig(REQUEST_CONFIG);
         CloseableHttpResponse response = client.execute(get);
-        return Result.getInstance(response);
+        return buildResult(response);
     }
 
     public Result get(URI uri) throws ClientProtocolException, IOException {
         HttpGet get = new HttpGet(uri);
+        get.setConfig(REQUEST_CONFIG);
         CloseableHttpResponse response = client.execute(get);
-        return Result.getInstance(response);
+        return buildResult(response);
     }
 
     public Result get(String uri, Map<String, String> params) throws ClientProtocolException,
@@ -58,8 +60,9 @@ public class HttpUnit {
 
     public Result post(String uri) throws ClientProtocolException, IOException {
         HttpPost post = new HttpPost(uri);
+        post.setConfig(REQUEST_CONFIG);
         CloseableHttpResponse response = client.execute(post);
-        return Result.getInstance(response);
+        return buildResult(response);
     }
 
     /**
@@ -81,9 +84,10 @@ public class HttpUnit {
         }
         UrlEncodedFormEntity entity = new UrlEncodedFormEntity(parameters, Charsets.UTF_8);
         HttpPost post = new HttpPost(uri);
+        post.setConfig(REQUEST_CONFIG);
         post.setEntity(entity);
         CloseableHttpResponse response = client.execute(post);
-        return Result.getInstance(response);
+        return buildResult(response);
     }
 
     /**
@@ -128,9 +132,10 @@ public class HttpUnit {
             }
         }
         HttpPost post = new HttpPost(uri);
+        post.setConfig(REQUEST_CONFIG);
         post.setEntity(entityBuilder.build());
         CloseableHttpResponse response = client.execute(post);
-        return Result.getInstance(response);
+        return buildResult(response);
     }
 
     /**
@@ -156,9 +161,20 @@ public class HttpUnit {
             }
         }
         HttpPost post = new HttpPost(uri);
+        post.setConfig(REQUEST_CONFIG);
         post.setEntity(entityBuilder.build());
         CloseableHttpResponse response = client.execute(post);
-        return Result.getInstance(response);
+        return buildResult(response);
     }
 
+    private static Result buildResult(CloseableHttpResponse response) throws IOException {
+        Result result = new Result();
+        result.setCode(response.getStatusLine().getStatusCode());
+        HttpEntity entity = response.getEntity();
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
+        entity.writeTo(bo);
+        result.setBody(new String(bo.toByteArray(), "utf8"));
+        response.close();
+        return result;
+    }
 }
--- petgang-commons/petgang-commons/src/main/java/com/petgang/commons/http/Result.java
+++ petgang-commons/petgang-commons/src/main/java/com/petgang/commons/http/Result.java
@@ -1,10 +1,5 @@
 package com.petgang.commons.http;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.http.client.methods.CloseableHttpResponse;
-
 public class Result {
 
     private int code;
@@ -25,17 +20,6 @@ public class Result {
 
     public void setBody(String body) {
         this.body = body;
-    }
-
-    public static Result getInstance(CloseableHttpResponse response) throws IllegalStateException,
-            IOException {
-        Result result = new Result();
-        InputStream in = response.getEntity().getContent();
-        result.setCode(response.getStatusLine().getStatusCode());
-        byte[] bytes = new byte[in.available()];
-        in.read(bytes);
-        result.setBody(new String(bytes));
-        return result;
     }
 
 }
